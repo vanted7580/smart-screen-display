9f70ae711b7069fd0c9e8830fa698063e687b4b9
VANTED <54582460+wqy224491@users.noreply.github.com>
2023-04-06 14:13:35 +0800

    Create uninstall.cmd

---
+++ Package Config/uninstall.cmd
@@ -0,0 +1,27 @@
+@echo off
+net.exe session 1>NUL 2>NUL && (
+    goto install
+) || (
+    echo Please run this script as Administrator.
+   goto end
+)
+
+:install
+
+cd /d "%~dp0"
+
+SET destination="C:\Program Files\Smart Screen Display\"
+SET cdestination="C:\ProgramData\Smart Screen Display\"
+SET program="Smart Screen Display.exe"
+SET config="Smart Screen Display.ini"
+SET taskxml="Smart Screen Display.xml"
+SET taskname="Smart Screen Display"
+SET taskuser="%ComputerName%\%UserName%"
+
+rd /s /q %destination%
+rd /s /q %cdestination%
+schtasks /delete /tn %taskname% /f
+echo Uninstalled
+
+:end
+pause
